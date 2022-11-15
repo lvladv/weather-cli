1126ad0e64e283d693e008e3f1e13fcdbdf91c2a
lvladv <mr.lvladv@yandex.ru>
2022-11-15 22:30:55 +0300

    init project

---
+++ weather.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { getArgs } from "./helpers/args.js";
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+  console.log(args);
+  if (args.h) {
+    // help
+  }
+  if (args.s) {
+    // save city
+  }
+  if (args.t) {
+    // token
+  }
+  // вывод погоды
+};
+
+initCLI();
